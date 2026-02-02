7f0f8671a68ddc52319b21b077338c25a9b64578
Vyacheslav Krakhmatov <zaramanias@gmail.com>
2026-02-02 16:00:44 +0100

    Исправлены данные в 1 задании

--- lesson_06/task1.py
+++ lesson_06/task1.py
@@ -2,15 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(service=ChromeService(
     ChromeDriverManager().install()))
 
-driver.implicitly_wait(16)
+waiter = WebDriverWait(driver, 20, 0.01)
 
 driver.get("http://uitestingplayground.com/ajax")
-
 driver.find_element(By.CSS_SELECTOR, "#ajaxButton").click()
+waiter.until(
+    EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#content"),
+                                     "Data loaded with AJAX get request.")
+)
 content = driver.find_element(By.CSS_SELECTOR, "#content")
 txt = content.find_element(By.CSS_SELECTOR, "p.bg-success").text
 print(txt)
